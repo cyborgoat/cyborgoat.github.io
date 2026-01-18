20680aa36cc75db104bf475d02e00c9b8532e147
cyborgoat <cyborgoat@outlook.com>
2026-01-18 09:38:55 +0800

    update: pkg versions

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
     images: {
         domains: [
             "images.unsplash.com",
-            "bbs-img.huaweicloud.com"
+            "bbs-img.huaweicloud.com",
+            "arxiv.org",
+            "pbs.twimg.com",
+            "media.springernature.com",
+            "cdn.sanity.io",
+            "upload.wikimedia.org",
             // Add other domains as needed
         ],
     },
